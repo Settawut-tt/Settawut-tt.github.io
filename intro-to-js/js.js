eb2224e9f1fe451895dea7a73d2bac0e9af6c839
Settawut Thaingthat <settawut.th@ku.th>
2022-02-17 14:12:33 +0700

    hw-js

---
+++ intro-to-js/js.js
@@ -0,0 +1,12 @@
+function askMyName() {
+    const name = prompt("ใส่ชื่อซะไอเวร"); // เรียกใช้คำสั่ง prompt เพื่อถามชื่อ https://www.w3schools.com/jsref/met_win_prompt.asp
+ 
+    if (name != null) {
+       document.getElementById("name").innerHTML = "My name is " + name;
+    }
+ }
+ 
+ function helloWorld() {
+ const h1message = document.getElementById("name").innerHTML;// ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
+    alert(h1message + ' ไม่อยากเสียวอย่าเลี้ยวหน้าพี่')
+ }
